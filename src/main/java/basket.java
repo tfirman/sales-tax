e9196d0f87fd2070aeeb3732a1304126bf7d3fcf
tfirman <tkfirman@gmail.com>
2019-01-03 18:57:12 -0800

    adds basket totals

--- src/main/java/basket.java
+++ src/main/java/basket.java
@@ -1,40 +1,53 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.*;
 
 public class basket {
-    private List<purchase> purchases;
-    private int taxTotal;
-    private int grandTotal;
+    private ArrayList<purchase> purchases;
+    //All monetary amounts are in integer cents.
+    private int totalTaxes;
+    private int totalPrice;
 
-    private void basket(String fileName){
+    private void basket(String fileName) {
         //In a more real-world situation, items might be identified by a PublicId or slug.
         //This exercise gave me input in plain text, so I'm going with that and parsing each input as a text file.
+        ArrayList<purchase> purchases = new ArrayList<purchase>();
         try {
             FileReader input = new FileReader(fileName);
             BufferedReader bufRead = new BufferedReader(input);
             String myLine = null;
 
-            while ( (myLine = bufRead.readLine()) != null)
-            {
+            while ((myLine = bufRead.readLine()) != null) {
+
                 // Assumptions must be made about input format.  I'm assuming here that all lines end with " at " followed by the price, as that's true in all provided examples.
-                // I'm also assuming no item names will include " at ".
+                // I'm also assuming no item names will include " at ", so there will only be one " at " in each line.
+
                 String[] array1 = myLine.split(" at ", 2);
+
                 // Another assumption I must make one way or the other as all examples provided are of exactly one unit:
                 // From context, I would guess that the phrase " at <price>" denotes a unit price rather than the aggregate price, but that's only a guess.
                 // It would be a high priority to verify that this was correct or to get any example with a multiple-unit purchase and total to verify.
                 // Otherwise, there would be a division by number of units needed.
-                int unitPrice = (int) Math.round (100. * Double.parseDouble(array1[1]));
+
+                int unitPrice = (int) Math.round(100. * Double.parseDouble(array1[1]));
+
                 // Here I'm relying on each line beginning with an integer number of units followed by a space, then the item description in text:
+
                 String[] array2 = array1[0].split(" ", 2);
                 int amount = Integer.parseInt(array2[0]);
                 String description = array2[1];
+
                 // Now to a very clumsy part.  In practice it would be vastly better to have a database where item PublicIds or slugs would correlate to an entry with booleans for the taxes;
                 // in theory, a sophisticated algorithm could try to figure out from strings and dictionaries whether arbitrary names counted as books, medical, or food products.
-                // I'm going to do a quick and dirty method here, in practice this should probably be a database call, if I had made a database.
+                // I'm going to do a quick and dirty method here of a brute force match check of known exempt (from examples) item descriptions.
+
                 String[] basicExemptNames = {"book", "chocolate bar", "imported box of chocolates", "packet of headache pills", "box of imported chocolates"};
                 Boolean basicExempt = Arrays.asList(basicExemptNames).contains(description);
+
                 // Also, I'm going to assume that all imported (and only imported) purchase descriptions contain the substring "imported". (this is true for the examples)
+
                 Boolean imported = description.contains("imported");
 
                 this.purchases.add(new purchase(amount, description, unitPrice, basicExempt, imported));
@@ -44,7 +57,24 @@ public class basket {
         } catch (IOException exception) {
             System.out.println("I/O error while reading " + fileName);
         }
+        int taxTally = 0;
+        int totalTally = 0;
+        for (int i = 0; i < this.purchases.size(); i++) {
+            taxTally += this.purchases.get(i).getTaxDue();
+            totalTally += this.purchases.get(i).getAmount() * this.purchases.get(i).getUnitPrice();
+        }
+        totalTally += taxTally;
+        this.totalTaxes = taxTally;
+        this.totalPrice = totalTally;
+
     }
+
+    public ArrayList<purchase> getPurchases() { return purchases; }
+
+    public int getTotalTaxes()  { return totalTaxes; }
+
+    public int getTotalPrice()  { return totalPrice; }
+
 
 
     public class purchase {
@@ -82,7 +112,7 @@ public class basket {
             return unitPrice;
         }
 
-        public void setBasePrice(Integer unitPrice) {
+        public void setUnitPrice(Integer unitPrice) {
             this.unitPrice = unitPrice;
         }
 
@@ -104,6 +134,7 @@ public class basket {
 
         public Integer getTaxDue() {
             Integer tax = 0;
+
             // the below calculation assumes that the two taxes are to be calculated independently.
             // The description of the problem does not make it clear if all the sales taxes are together rounded up to the nearest 0.05, or if each of them are to be rounded individually.
             // The code above assumes that the rounding is separate, but in practice I'd ask or possibly research the local law to see which was needed.
